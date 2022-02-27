4de5b05a8c0cd8413ae0876016db6d2269b5c67a
Darshan Thorat <darshaanthorat17@gmail.com>
2022-02-27 14:50:26 +0530

    LinkQ database

---
+++ LinkQdemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkQdemo;
+
+Console.WriteLine("Hello, World!");
+
+
+// Console.WriteLine("Welcome to Data Table demo");
+//LinqToDataTable linqToDataTable = new LinqToDataTable();
+//linqToDataTable.AddToDataTableDemo();
+Console.ReadKey();
---
+++ ProductReviewManagementWithLinq/ProductidReviews.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementWithLinq
+{
+    internal class ProductidReviews
+    {
+        public void PidAndReviews(List<ProductReview> IDReviews)
+        {
+            var recordedData = IDReviews.GroupBy((y, z) => (y.ProducID, z.Review)).Select(x => new { ProductID = x.Key, Reviews = x.Key });
+
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine(list.ProductID + "------------" + list.Review);
+
+
+            }
+        }
+    }
+}
